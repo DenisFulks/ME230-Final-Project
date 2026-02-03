813dfc9a52471d034b6bb27fffb09cc920b9b8bc
DenisFulks <denisfulks@gmail.com>
2026-02-03 07:57:24 -0500

    Code

--- Code/ClawMachine/ClawMachine.ino
+++ Code/ClawMachine/ClawMachine.ino
@@ -1,26 +1,39 @@
+#include <Servo.h>
+
 // Motor Pins
 #define pinXPos 48
 #define pinXNeg 49
 #define pinYPos 46
 #define pinYNeg 47
 
+// Claw Pins
+#define spoolUpPin 50
+#define spoolDownPin 51
+#define servoPin 2
+
 // Input Pins
-#define pinEStop 2
+#define pinClaw 21
 #define pinJoyX A0
 #define pinJoyY A1 
 
-// Other Pins
-#define xEnable 22
-#define yEnable 23
+// Flags
+volatile uint8_t mainEventFlags = 0x00;
+#define dropClawFlag 1
 
 // Constants
 const uint8_t deadZone = 40;
 
 // Variable Defenitions
-uint8_t EStop = 0;
 int16_t xVal;
 int16_t yVal;
 int direction[2] = {0, 0};
+
+// Claw
+Servo claw;
+uint8_t open = 0;
+uint8_t close = 180;
+
+unsigned int downTime = 1.2; // s
 
 void setup() {
   Serial.begin(9600);
@@ -31,21 +44,22 @@ void setup() {
   pinMode(pinYPos, OUTPUT);
   pinMode(pinYNeg, OUTPUT);
 
-  // Enable Pins
-  pinMode(xEnable, OUTPUT);
-  pinMode(yEnable, OUTPUT);
+  // Setup Claw Servo
+  claw.attach(servoPin);
+  claw.write(close);
 
-  // E-Stop Interrupt
-  pinMode(pinEStop, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(pinEStop), EmergencyStop, FALLING);
-
-  // Closing Setup
-  sei();
-  digitalWrite(xEnable, HIGH);
-  digitalWrite(yEnable, HIGH);
+  // Claw Interrupt
+  pinMode(pinClaw, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinClaw), dropClaw, FALLING);
 }
 
 void loop() {
+  if (mainEventFlags & dropClawFlag) {
+    Serial.println("Test");
+
+    pickup();
+  }
+
   readJoyStick();
   moveGantry();
 }
@@ -73,7 +87,7 @@ void readJoyStick() {
     direction[1] = 0;
   }
 
-  Serial.println(String(direction[0]) + String(", ") + String(direction[1]));
+  Serial.println(direction[1]);
 }
 
 void moveGantry() {
@@ -102,18 +116,26 @@ void moveGantry() {
   }
 }
 
-void EmergencyStop() {
-  if (!EStop){
-    Serial.println(String("Stopping"));
+void pickup() {
+  claw.write(open);
 
-    digitalWrite(xEnable, LOW);
-    digitalWrite(yEnable, LOW);
-  } else {
-    Serial.println(String("Starting"));
+  digitalWrite(spoolDownPin, HIGH);
+  delay(downTime * 1000);
+  digitalWrite(spoolDownPin, LOW);
 
-    digitalWrite(xEnable, HIGH);
-    digitalWrite(yEnable, HIGH);
+  delay(200);
+  claw.write(close);
+  delay(100);
+
+  digitalWrite(spoolUpPin, HIGH);
+  delay(downTime * 1000);
+  digitalWrite(spoolUpPin, LOW);
+
+  mainEventFlags &= ~dropClawFlag;
+}
+
+void dropClaw() {
+  if (!(mainEventFlags & dropClawFlag)) {
+    mainEventFlags |= dropClawFlag;
   }
-
-  EStop = !EStop;
 }
