892f6bc04cefc2bb3d864ab62da4650f83a9961a
DenisFulks <denisfulks@gmail.com>
2026-02-16 09:22:34 -0500

    Hopefully Done

--- Code/ClawMachine/ClawMachine.ino
+++ Code/ClawMachine/ClawMachine.ino
@@ -1,4 +1,6 @@
 #include <Servo.h>
+#include <LiquidCrystal.h>
+#include <string.h>
 
 // Motor Pins
 #define pinXPos 48
@@ -20,6 +22,7 @@
 // Input Pins
 #define pinClaw 21
 #define EStopPin 20
+#define winPin 19
 #define pinJoyX A0
 #define pinJoyY A1
 #define photoResistorPin A2
@@ -28,10 +31,11 @@
 volatile uint8_t mainEventFlags = 0x00;
 #define dropClawFlag 1
 #define EStopFlag 2
+#define winFlag 4
 
 // Constants
 const uint8_t deadZone = 40;
-const uint8_t photoResistorCutoff = 50;
+const uint8_t photoResistorCutoff = 100;
 const uint8_t maxTime = 20; // Seconds
 
 // Variable Defenitions
@@ -39,18 +43,21 @@ int16_t xVal;
 int16_t yVal;
 int direction[2] = {0, 0};
 bool play = false;
-int16_t startTime;
-int16_t endTime;
+long int startTime;
 
 // Claw
 Servo claw;
 uint8_t open = 0;
 uint8_t close = 180;
 
-unsigned float downTime = 1.5; // Seconds
+float downTime = 1.5; // Seconds
+
+// LCD
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
 
   // Motor Pins
   pinMode(pinXPos, OUTPUT);
@@ -76,29 +83,42 @@ void setup() {
   pinMode(EStopPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(EStopPin), EStop, FALLING);
 
+  // win Interrupt
+  pinMode(winPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(winPin), win, FALLING);
+
   goHome();
+  drop();
 }
 
 void loop() {
   if (!play) {
-      if (analogRead(photoResistorPin) < photoResistorCutoff) {
+    //Serial.println(analogRead(photoResistorPin));
+    digitalWrite(pinXPos, LOW);
+    digitalWrite(pinXNeg, LOW);
+    digitalWrite(pinYPos, LOW);
+    digitalWrite(pinYNeg, LOW);
+
+    if (analogRead(photoResistorPin) < photoResistorCutoff) {
       while (analogRead(photoResistorPin) < photoResistorCutoff);
 
       Serial.println("Play!");
+      updateLCD("Play!", "");
+      delay(500);
 
-      startTime = millis();
-      endTime = startTime + (maxTime*1000);
+      startTime = millis() / 1000;
       play = true;
+
+      Serial.print(startTime); Serial.print(", "); Serial.println(maxTime);
     }
-  } else { 
+
+    updateLCD("Insert Coin", "To Play");
+  } else {
+    Serial.println(maxTime - (millis()/1000 - startTime));
+    updateLCD(String("Time Left: "), String(maxTime - (millis()/1000 - startTime)));
+
     readJoyStick();
-
-    if (mainEventFlags & dropClawFlag) {
-      pickup();
-      goHome();
-      drop();
-    }
-
+    
     if ((!digitalRead(xMinPin) && direction[0] < 0) || (!digitalRead(xMaxPin) && direction[0] > 0)) {
       direction[0] = 0;
     }
@@ -109,10 +129,22 @@ void loop() {
 
     moveGantry();
 
-    if (startTime + millis() > endTime) {
+    if (millis()/1000 - startTime >= maxTime) {
+      Serial.println("Times Up!");
+      Serial.print(millis()/1000 - startTime); Serial.print(", "); Serial.println(maxTime);
+      updateLCD("Time's Up!", "");
+
       pickup();
       goHome();
       drop();
+    }
+
+    if (mainEventFlags & dropClawFlag) {
+      pickup();
+      goHome();
+      drop();
+
+      mainEventFlags &= ~dropClawFlag;
     }
   }
 }
@@ -142,6 +174,8 @@ void readJoyStick() {
 }
 
 void moveGantry() {
+  // Serial.print(direction[0]);Serial.println(direction[1]);
+  
   // X-Direction
   if (direction[0] > 0) {
     digitalWrite(pinXNeg, LOW);
@@ -168,6 +202,14 @@ void moveGantry() {
 }
 
 void pickup() {
+  Serial.println("Picking Up!");
+  updateLCD("Down we go!", "");
+
+  digitalWrite(pinXPos, LOW);
+  digitalWrite(pinXNeg, LOW);
+  digitalWrite(pinYPos, LOW);
+  digitalWrite(pinYNeg, LOW);
+
   claw.write(open);
 
   digitalWrite(spoolDownPin, HIGH);
@@ -179,24 +221,30 @@ void pickup() {
   delay(100);
 
   digitalWrite(spoolUpPin, HIGH);
-  delay(downTime * 1000);
+  delay((downTime + 0.05) * 1000);
   digitalWrite(spoolUpPin, LOW);
 
   mainEventFlags &= ~dropClawFlag;
 }
 
 void drop() {
+  digitalWrite(pinXPos, LOW);
+  digitalWrite(pinXNeg, LOW);
+  digitalWrite(pinYPos, LOW);
+  digitalWrite(pinYNeg, LOW);
+
   claw.write(open);
-  delay(200);
+  delay(500);
 
   claw.write(close);
   delay(100);
 }
 
 void goHome() {
-  while (digitalRead(xMinPin) || digitalRead(yMinPin)) {
-    Serial.println("Going Home");
+  Serial.println("Going Home");
+  updateLCD("Did you Win?", "");
 
+  while (digitalRead(xMinPin) || digitalRead(yMinPin)) {
     if (digitalRead(xMinPin)) {
       direction[0] = -1;
     } else {
@@ -206,19 +254,58 @@ void goHome() {
     if (digitalRead(yMinPin)) {
       direction[1] = -1;
     } else {
-      direction[1] = 1;
+      direction[1] = 0;
     }
 
     moveGantry();
   }
 
+  digitalWrite(pinXPos, LOW);
+  digitalWrite(pinXNeg, LOW);
+  digitalWrite(pinYPos, LOW);
+  digitalWrite(pinYNeg, LOW);
+  Serial.println("Home!");
+
   play = false;
+}
+
+void checkWin() {
+  startTime = millis()/1000;
+
+  while (millis()/1000 - startTime < 3) {
+      if (mainEventFlags & winFlag) {
+      updateLCD("Winner!", "");
+      delay(2000);
+
+      mainEventFlags &= ~winFlag;
+
+      return;
+    }
+  }
+
+  updateLCD("loser!", "");
+  delay(2000);
+}
+
+void updateLCD(String topMessage, String lowMessage) {
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print(topMessage);
+
+  lcd.setCursor(0, 1);
+  lcd.print(lowMessage);
+
 }
 
 void dropClaw() {
   if (!(mainEventFlags & dropClawFlag)) {
     mainEventFlags |= dropClawFlag;
   }
+}
+
+void win() {
+  mainEventFlags |= winFlag;
 }
 
 void EStop() {
